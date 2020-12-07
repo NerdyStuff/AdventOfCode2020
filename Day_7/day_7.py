95496b32bf053220a4bda118ca1cd86dbd65dd04
NerdyStuff <NerdyStuff@users.noreply.github.com>
2020-12-07 13:45:31 +0100

    Add files via upload

--- Day_7/day_7.py
+++ Day_7/day_7.py
@@ -1,5 +1,12 @@
 file = open("luggage_rules.txt", "r")
 
+# TODO => NEEDS TO BE REFACTORED FOR INPUT
+def findColor(searchColor, dictionary):
+    foundKeys = []
+    if any(searchColor in string for string in bagList) == True:
+        foundKeys.append(dictionary[color])
+    return foundKeys
+
 rules = dict()
 for line in file:
     line = line[:-2]
@@ -14,4 +21,8 @@ for line in file:
     rules[bagColor] = bags
     bags = []
 
-print(rules)
+liste = []
+for color, bagList in rules.items():
+    if any("shiny gold" in string for string in bagList) == True:
+        liste.append({color: rules[color]})
+print(liste)
